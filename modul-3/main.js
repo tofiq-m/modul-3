4f70fca3c3d3b9dc7d39963753231f29c31f6ede
Tofiq-m <99548128+tofiq-m@users.noreply.github.com>
2022-04-25 21:22:08 +0400

    Almost done

--- modul-3/main.js
+++ modul-3/main.js
@@ -2,8 +2,10 @@ let input = document.querySelectorAll('.input');
 let currencies = document.querySelectorAll('.currencies');
 let curr1 = currencies[0].children;
 let curr2 = currencies[1].children;
-
-
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let from = document.getElementById("from");
+let to = document.getElementById("to");
 // STYLING--------------------------------
 
 for (let i = 0; i < curr1.length; i++) {
@@ -33,17 +35,39 @@ for (let i = 0; i < curr2.length; i++) {
 function getData() {
     let selected1 = document.querySelector('.selected1').innerHTML;
     let selected2 = document.querySelector('.selected2').innerHTML;
-    fetch(`https://api.exchangerate.host/latest?base=${selected1}&symbols=${selected2}`)
+    fetch(`https://api.exchangerate.host/latest?base=${selected1}&symbols=RUB,USD,EUR,GBP`)
         .then(response => response.json())
-        .then(data => {
-            console.log(data);
+        .then(data => {            
+            if (selected2 == 'RUB') {
+                let amount = input1.value * data.rates.RUB; 
+                input2.value = amount;
+                from.innerHTML = `1 ${selected1} = ${ data.rates.RUB} ${selected2}`;
+                to.innerHTML = `1 ${selected2} = ${1/ data.rates.RUB} ${selected1}`;
+            }
+            if (selected2 == 'USD') {
+                let amount = input1.value * data.rates.USD;
+                input2.value = amount;
+                from.innerHTML = `1 ${selected1} = ${ data.rates.USD} ${selected2}`;
+                to.innerHTML = `1 ${selected2} = ${1/ data.rates.USD} ${selected1}`;
+            }
+            if (selected2 == 'EUR') {
+                let amount = input1.value * data.rates.EUR;
+                input2.value = amount;
+                from.innerHTML = `1 ${selected1} = ${ data.rates.EUR} ${selected2}`;
+                to.innerHTML = `1 ${selected2} = ${1/ data.rates.EUR} ${selected1}`;
+            }
+            if (selected2 == 'GBP') {
+                let amount = input1.value * data.rates.GBP;
+                input2.value = amount;
+                from.innerHTML = `1 ${selected1} = ${ data.rates.GBP} ${selected2}`;
+                to.innerHTML = `1 ${selected2} = ${1/ data.rates.GBP} ${selected1}`;
+            };
         })
         .catch(err => console.error(err));
-}
+}  
 
 for (let i = 0; i < curr1.length; i++) {
     curr1[i].addEventListener('click', () => {
-        // console.log(curr1[i].classList.contains("selected1"));
         if (!(curr1[i].classList.contains("selected1"))) {
             for (let j = 0; j < curr1.length; j++) {
                 curr1[j].classList.remove('selected1');
