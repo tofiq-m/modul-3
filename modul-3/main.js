fba144e399f8cfd4d6c36ba413a0372e491d5b93
Tofiq-m <99548128+tofiq-m@users.noreply.github.com>
2022-04-26 19:35:25 +0400

    typing only number no input and adding toFixed

--- modul-3/main.js
+++ modul-3/main.js
@@ -6,7 +6,7 @@ let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
 let from = document.getElementById("from");
 let to = document.getElementById("to");
-// STYLING--------------------------------
+// STYLING----------------------------------
 
 for (let i = 0; i < curr1.length; i++) {
     curr1[i].addEventListener('click', () => {
@@ -31,33 +31,32 @@ for (let i = 0; i < curr2.length; i++) {
 };
 
 //FUNTIONALITY--------------------------------------
-
 function getData() {
     let selected1 = document.querySelector('.selected1').innerHTML;
     let selected2 = document.querySelector('.selected2').innerHTML;
     fetch(`https://api.exchangerate.host/latest?base=${selected1}&symbols=RUB,USD,EUR,GBP`)
         .then(response => response.json())
-        .then(data => {            
+        .then(data => {        
             if (selected2 == 'RUB') {
-                let amount = input1.value * data.rates.RUB; 
+                let amount = (input1.value * data.rates.RUB).toFixed(4); 
                 input2.value = amount;
                 from.innerHTML = `1 ${selected1} = ${ data.rates.RUB} ${selected2}`;
                 to.innerHTML = `1 ${selected2} = ${1/ data.rates.RUB} ${selected1}`;
             }
             if (selected2 == 'USD') {
-                let amount = input1.value * data.rates.USD;
+                let amount = (input1.value * data.rates.USD).toFixed(4);
                 input2.value = amount;
                 from.innerHTML = `1 ${selected1} = ${ data.rates.USD} ${selected2}`;
                 to.innerHTML = `1 ${selected2} = ${1/ data.rates.USD} ${selected1}`;
             }
             if (selected2 == 'EUR') {
-                let amount = input1.value * data.rates.EUR;
+                let amount = (input1.value * data.rates.EUR).toFixed(4);
                 input2.value = amount;
                 from.innerHTML = `1 ${selected1} = ${ data.rates.EUR} ${selected2}`;
                 to.innerHTML = `1 ${selected2} = ${1/ data.rates.EUR} ${selected1}`;
             }
             if (selected2 == 'GBP') {
-                let amount = input1.value * data.rates.GBP;
+                let amount = (input1.value * data.rates.GBP).toFixed(4);
                 input2.value = amount;
                 from.innerHTML = `1 ${selected1} = ${ data.rates.GBP} ${selected2}`;
                 to.innerHTML = `1 ${selected2} = ${1/ data.rates.GBP} ${selected1}`;
